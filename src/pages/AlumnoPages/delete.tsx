7d6ac65ec6008329a125fc901975f6fea2fd853a
urielxdov <urieledgar878@gmail.com>
2025-07-13 14:49:44 -0600

    Coreccion del delete

--- src/pages/AlumnoPages/delete.tsx
+++ src/pages/AlumnoPages/delete.tsx
@@ -1,37 +1,31 @@
-import Delete from "../CrudActions/Delete";
-import HomeLayout from "../Layouts/HomeLayout";
-import ReturnButton from "../../componets/utils/buttons/ReturnButton";
-import { ALUMNOHEADERS } from "../../utils/Headers";
-import { useStudents } from "../../hooks/custom/useStudents";
+import Delete from '../CrudActions/Delete'
+import HomeLayout from '../Layouts/HomeLayout'
+import ReturnButton from '../../componets/utils/buttons/ReturnButton'
+import { ALUMNOHEADERS } from '../../utils/Headers'
+import { useContext } from 'react'
+import { StudentsContext } from '../../hooks/context/StudentContext'
 
-function DeleteAlu(){
-    //contexto de alumno
-    const context = useStudents();
+function DeleteAlu () {
+  //contexto de alumno
+  const context = useContext(StudentsContext)
 
-    //manejo de eliminacion
-    const drop = () => {
-        //obtener alumno
-        const alumno = context.state.student;
+  if (!context) return <div>Error: contexto no disponible</div>
 
-        //validacion de existencia (nadamas para que ts no llore)
-        if(alumno){
-            context.deleteStudent(alumno.id);
-        }
-    }
+  const { state, deleteStudent, searchStudent } = context
 
-    //retorno de componente
-    return(
-        <HomeLayout title="Modulo Alumno">
-            <Delete 
-                module="alumno"
-                headers={ALUMNOHEADERS}
-                entity={context.state.student}
-                onSearch={context.searchStudent}
-                onDelete={drop}
-            />
-            <ReturnButton path="/alumno/"/>
-        </HomeLayout>
-    );
+  //retorno de componente
+  return (
+    <HomeLayout title='Modulo Alumno'>
+      <Delete
+        headers={ALUMNOHEADERS}
+        entity='alumno'
+        body={state.students}
+        onSearch={(numberControl: string) => searchStudent(numberControl)}
+        onDelete={(numberControl: string) => deleteStudent(numberControl)}
+      />
+      <ReturnButton path='/alumno/' />
+    </HomeLayout>
+  )
 }
 
-export default DeleteAlu;
+export default DeleteAlu
--- src/pages/CrudActions/Delete.tsx
+++ src/pages/CrudActions/Delete.tsx
@@ -1,37 +1,55 @@
-import { type DeleteParameters } from "../../interfaces/CRUDInterfaces";
-import QueryInput from "../../componets/utils/Inputs/QueryInput";
-import Table from "../../componets/tables/Table";
-import Button from "../../componets/utils/buttons/Button";
-import { parseObjectToRow } from "../../utils/ParserObjects";
+import Table from '../../componets/tables/Table'
+import QueryInput from '../../componets/utils/Inputs/QueryInput'
+import type {
+  AlumnoConfig,
+  ProfesorConfig,
+  ProgramaConfig
+} from '../../interfaces/ModelsInterfaces'
 
-function Delete({module, headers, entity, onSearch, onDelete}:DeleteParameters){
-    //validar entidad
-    if(!entity){
-        return(
-          <>
-            <QueryInput placeholder={"buscar "+module} action={onSearch}/>
-            Sin registros
-          </>
-        );
-    }else{
-        return(
-          <>
-            <QueryInput placeholder={"buscar "+module} action={onSearch}/>
-            <Table
-              header={headers}
-              body={[parseObjectToRow(entity)]}
-            />
-            <Button
-              text="eliminar"
-              action={onDelete}
-              submit={false}
-              styles="p-1 text-white bg-red-500 rounded-sm
-                    hover: bg-red-600 hover: cursor-pointer
-                    "
-            />
-          </>  
-        );
-    }
+type PropsDeletePage = {
+  entity: string
+  headers: string[]
+  body: AlumnoConfig[] | ProfesorConfig[] | ProgramaConfig[] // modelos
+  onSearch: (value: string) => void
+  onDelete: (id: string) => void
 }
 
-export default Delete;
+export function Delete ({
+  entity,
+  headers,
+  body,
+  onDelete,
+  onSearch
+}: PropsDeletePage) {
+  const adaptedData = () => {
+    const mappedData: React.ReactNode[][] = []
+    body.forEach(element => {
+      const row: React.ReactNode[] = []
+
+      Object.keys(element).forEach(key => {
+        row.push(element[key as keyof typeof element])
+      })
+      row.push(
+        <button
+          key='delete'
+          onClick={() => onDelete(element.id)}
+          className='w-full bg-red-600 text-white rounded-sm p-2 hover:cursor-pointer hover:bg-red-700'
+        >
+          Dar baja
+        </button>
+      )
+
+      mappedData.push(row)
+    })
+    return mappedData
+  }
+
+  return (
+    <>
+      <QueryInput placeholder={`Buscar ${entity}`} action={onSearch} />
+      <Table header={headers.concat('Acciones')} body={adaptedData()} />
+    </>
+  )
+}
+
+export default Delete
