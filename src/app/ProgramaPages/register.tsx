9ae80e6f5333d0e456c9dbcd158a4f1d7e0d495b
urielxdov <urieledgar878@gmail.com>
2026-06-03 17:21:55 -0600

    feat: migrate Programa pages to TanStack Query

--- src/app/ProgramaPages/register.tsx
+++ src/app/ProgramaPages/register.tsx
@@ -1,75 +1,43 @@
-import Create from "../CrudActions/Create";
-import HomeLayout from "../../components/ui/HomeLayout";
-import ReturnButton from "../../components/interactives/buttons/ReturnButton";
-import { getFieldsProg } from "../../utils/Fields";
-import { usePrograms } from "../../hooks/context/ProgramContext";
-import { useForm } from "../../hooks/reducers/FormReducer";
-import type { ProgramaModel } from "../../interfaces/Models";
-import Modal from "../../components/ui/Modals";
-import { useState } from "react";
-import debounce from "../../utils/Debounce";
-import { useAuth } from "../../hooks/context/AuthContext";
+import Create from "../CrudActions/Create"
+import HomeLayout from "../../components/ui/HomeLayout"
+import ReturnButton from "../../components/interactives/buttons/ReturnButton"
+import { getFieldsProg } from "../../utils/Fields"
+import { useForm } from "../../hooks/reducers/FormReducer"
+import type { ProgramaModel } from "../../interfaces/Models"
+import Modal from "../../components/ui/Modals"
+import { useState } from "react"
+import debounce from "../../utils/Debounce"
+import { useCreateProgram } from "../../hooks/mutations/useProgramMutations"
 
-function CreateProg(){
-    //hook de jwt
-    const jwt = useAuth();
+function CreateProg() {
+    const [openSuccess, setOpenSuccess] = useState(false)
+    const [openFail, setOpenFail] = useState(false)
+    const { state, handleChange, resetForm } = useForm('Programa')
+    const { mutate: create } = useCreateProgram()
 
-    //estado de modal
-    const [openSuccess,setOpenSuccess] = useState<boolean>(false);
-    const [openFail,setOpenFail] = useState<boolean>(false);
-
-    //uso del contexto
-    const context = usePrograms();
-
-    //hook de formulario
-    const { state, handleChange, resetForm } = useForm('Programa');
-
-    //manejo de datos
     const submit = debounce(() => {
-        //obtener el modelo
         const programa = state.data as ProgramaModel
-        //activo por defecto
         programa.status = "Activo"
-        //guardado en el contexto
-        context.addProgram(programa, jwt.token).then(created => {
-            if(created){
-                //mostrar el modal
-                setOpenSuccess(true);
-            }else{
-                setOpenFail(true);
-            }
-            resetForm();
-        }).catch(e => console.log(e));
-    },500);
+        create(programa, {
+            onSuccess: (result) => {
+                if (result.success) setOpenSuccess(true)
+                else setOpenFail(true)
+                resetForm()
+            },
+            onError: () => { setOpenFail(true); resetForm() }
+        })
+    }, 500)
 
-    //retorno de la vista
     return (
         <>
-        <HomeLayout title={"Modulo programa"}>
-            <Create
-                module="programa"
-                fields={getFieldsProg(state.data as ProgramaModel)}
-                onSubmit={submit}
-                onChange={handleChange}
-            />
-            <ReturnButton path="/programa/"/>
-        </HomeLayout>
-        <Modal
-            title="Programa creado"
-            message="el programa ha sido creado con exito"
-            type="success"
-            isOpen={openSuccess}
-            onClose={() => setOpenSuccess(false)}
-        />
-        <Modal
-            title="Error al crear"
-            message="el programa no ha sido creado"
-            type="failure"
-            isOpen={openFail}
-            onClose={() => setOpenFail(false)}
-        />
+            <HomeLayout title="Modulo programa">
+                <Create module="programa" fields={getFieldsProg(state.data as ProgramaModel)} onSubmit={submit} onChange={handleChange} />
+                <ReturnButton path="/programa/" />
+            </HomeLayout>
+            <Modal title="Programa creado" message="el programa ha sido creado con exito" type="success" isOpen={openSuccess} onClose={() => setOpenSuccess(false)} />
+            <Modal title="Error al crear" message="el programa no ha sido creado" type="failure" isOpen={openFail} onClose={() => setOpenFail(false)} />
         </>
-    );
+    )
 }
 
-export default CreateProg;
+export default CreateProg
